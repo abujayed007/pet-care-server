e3e94ab79898426d75144992e178d3fe509dd3a0
unknown <abujayed007@gmail.com>
2024-06-05 16:56:31 +0600

    railway

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
-const port = process.env.PORT  || 5000
+const port = 5000
 
 app.use(cors())
 app.use(express.json())
